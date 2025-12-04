04d22462c249bb3085857daa12906922e8c84408
jad nehme <jadnehme@jads-MacBook-Prolocal.local>
2025-12-04 15:55:11 +0200

    test 1

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,4 @@ const server = http.createServer((req, res) => {
   res.end('Hello DevOps!');
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+server.listen(3000, () => console.log('Server is running on port 3000'));
